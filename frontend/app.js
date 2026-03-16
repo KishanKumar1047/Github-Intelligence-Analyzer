96b2c8d2ecc3fb127751128bb6cbd56246d18017
= <=>
2026-03-17 00:30:29 +0530

    Add frontend

--- frontend/app.js
+++ frontend/app.js
@@ -2,7 +2,7 @@ async function analyze(){
 
 const username = document.getElementById("username").value
 
-const response = await fetch(`http://127.0.0.1:8000/analyze/${username}`)
+const response = await fetch(`https://github-intelligence-analyzer.onrender.com/analyze/${username}`)
 
 const data = await response.json()
 
